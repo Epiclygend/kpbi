0ccbd5e03e9c86433e42521bae1368f1a17b0a5e
Epiclygend <36941633+Epiclygend@users.noreply.github.com>
2020-09-18 01:56:14 +0700

    feat (vue): Add vuejs
    - Vuejs javascript project init
    - webpack update
    - laravel route
    
    others:
    - add RestAPI Helper
    - add image asset

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <base href="{{ url('') }}">
+
+        <title>@yield('title', config('app.name') )</title>
+        <link rel="shortcut icon" href="{{ asset('img/ICON_KPBI_no-text.png') }}" type="image/x-icon">
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="app" />
+    </body>
+</html>
